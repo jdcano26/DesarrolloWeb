66143d7f5d0215af61a23f91eb810078040e4c1e
jdcano26 <jesusdavid.canoramirez@gmail.com>
2025-10-27 23:59:13 -0500

    Se ajusta modelo y controlador

--- private/model/Product.js
+++ private/model/Product.js
@@ -1,7 +1,6 @@
 //Package Importation
 //System
 import Mongoose from 'mongoose'
-import Fs from 'fs'
 //Local
 import { dbConn } from '../../dbconn.js'
 import { Validation } from '../helpers/fieldValidation.js'
@@ -51,79 +50,19 @@ export class ProductModel {
             const split = this.split; // Separador para formatear mensajes de error o resultados.
 
             try {
-                var error = 0; // Variable para almacenar el último error encontrado durante las validaciones.
-    
-                // Validar imagen
-                // Comprueba que sea una imagen y no esté vacío el campo.
-                await validation.image(varImage, function (err) {
-                    if (err != null) {
-                        error = err; // Si hay un error, se almacena.
-                    }
-                });
-
-                // Validar especificaciones
-                // Verifica que el campo especificaciones cumpla con cantidad de carácteres.
-                validation.text(varDescription, 1000, "Especificaciones", "addSerieFormDescription", function (err) {
-                    if (err != null) {
-                        error = err; // Si hay un error, se almacena.
-                    }
-                });
-
-                // Validar precio
-                // Verifica que el campo precio cumpla con cantidad de carácteres.
-                validation.text(varPrice, 1000, "Precio", "addProductPrice", function (err) {
-                    if (err != null) {
-                        error = err; // Si hay un error, se almacena.
-                    }
-                });
-    
-                // Validar nombre 
-                // Comprueba que el nombre de usuario cumpla con las reglas establecidas (por ejemplo, formato y longitud).
-                validation.text(varName, 244, "Nombre del Celular", "addProductName", function (err) {
-                    if (err != null) {
-                        error = err; // Si hay un error, se almacena.
-                    }
-                });
                 
-                console.log(varName)
-                console.log(error)
+                // 2. Insertar en la Colección Producto
+                const addProduct = new ProductSchema({
+                    name: varName,
+                    price: varPrice,
+                    description: varDescription,
+                    image: './' + varImage
+                });
+    
+                await addProduct.save();
+    
+                callback(`1${split}Producto insertado correctamente...${split}/${split}2000`);
 
-                // Si no hay errores en ninguna validación, procedemos a crear el usuario
-                if (error == 0) {
-    
-                    const root = validation.root()
-                    const route = validation.route("product")
-                    const token = validation.token(10)
-                    const ext = validation.ext()
-    
-    
-                    const savePath = route + token + ext;
-                    const savePath2 = savePath.substring(root)
-    
-                    // Guardar el buffer en la ruta específica
-                    Fs.writeFile(savePath, varImage, (err) => {
-                        if (err) {
-                            console.error('Error al guardar la imagen:', err);
-                        } else {
-                            console.log('Imagen guardada exitosamente');
-                        }
-                    });
-    
-                    // 2. Insertar en la Colección Producto
-                    const addProduct = new ProductSchema({
-                        name: varName,
-                        price: varPrice,
-                        description: varDescription,
-                        image: './' + savePath2
-                    });
-    
-                    await addProduct.save();
-    
-                    callback(`1${split}Producto insertado correctamente...${split}/${split}2000`);
-                } else {
-                    // Si hubo errores durante las validaciones, devolver el último error encontrado
-                    callback(error);
-                }
             } catch (error) {
                 // Manejar cualquier error inesperado durante la consulta.
                 callback(`Se ha presentado un error debido a ${error.message}. Por favor inténtelo más tarde`);
--- src/controller/product.js
+++ src/controller/product.js
@@ -2,25 +2,21 @@
 //System
 import { Router } from 'express'
 import Multer from 'multer'
+import Fs from 'fs'
 //Local
 import { ProductModel } from '../../private/model/Product.js'
+import { Validation } from '../../private/helpers/fieldValidation.js'
 
 
 //Initialization
 const upload = Multer();
+//Local
+const validation = new Validation()
 
 //Server
 export const productRoute = Router()
 //Local
 const productModel = new ProductModel()
-/*
-var productData = {
-    userName: null,
-    userTheme: null,
-    userProfile: null,
-    userBackground: null
-}
-*/
 
 
 //Routes
@@ -30,14 +26,86 @@ productRoute.get('/product', async (req, res) => {
     })
 })
 productRoute.post('/formAddProduct', upload.single('addProductImage'), (req, res) => {
-    productModel.add(
-        req.body.addProductName,
-        req.body.addProductDescription,
-        req.body.addProductPrice,
-        req.file ? req.file.buffer : null,
-        async function( result ) {
-            
-            res.end( result )
+    const split = validation.splitValue()
+
+    try {
+        var error = 0; // Variable para almacenar el último error encontrado durante las validaciones.
+        var varName = req.body.addProductName
+        var varPrice = req.body.addProductPrice
+        var varDescription = req.body.addProductDescription
+        var varImage = req.file ? req.file.buffer : null
+
+        // Validar imagen
+        // Comprueba que sea una imagen y no esté vacío el campo.
+        validation.image(varImage, function (err) {
+            if (err != null) {
+                error = err; // Si hay un error, se almacena.
+            }
+        });
+    
+        // Validar especificaciones
+        // Verifica que el campo especificaciones cumpla con cantidad de carácteres.
+        validation.text(varDescription, 1000, "Especificaciones", "addSerieFormDescription", function (err) {
+            if (err != null) {
+                error = err; // Si hay un error, se almacena.
+            }
+        });
+    
+        // Validar precio
+        // Verifica que el campo precio cumpla con cantidad de carácteres.
+        validation.text(varPrice, 1000, "Precio", "addProductPrice", function (err) {
+            if (err != null) {
+                error = err; // Si hay un error, se almacena.
+            }
+        });
+        
+        // Validar nombre 
+        // Comprueba que el nombre de usuario cumpla con las reglas establecidas (por ejemplo, formato y longitud).
+        validation.text(varName, 244, "Nombre del Celular", "addProductName", function (err) {
+            if (err != null) {
+                error = err; // Si hay un error, se almacena.
+            }
+        });
+    
+        console.log(error)
+
+        // Si no hay errores en ninguna validación, procedemos a crear el usuario
+        if (error == 0) {
+        
+            const root = validation.root()
+            const route = validation.route("product")
+            const token = validation.token(10)
+            const ext = validation.ext()
+        
+        
+            const savePath = route + token + ext;
+            const savePath2 = savePath.substring(root)
+        
+            // Guardar el buffer en la ruta específica
+            Fs.writeFile(savePath, varImage, (err) => {
+                if (err) {
+                    console.error('Error al guardar la imagen:', err);
+                } else {
+                    productModel.add(
+                        varName,
+                        varDescription,
+                        varPrice,
+                        savePath2,
+                        async function( result ) {
+                            
+                            res.end( result )
+                        }
+                    )
+                }
+            });
+        
+        } else {
+            // Si hubo errores durante las validaciones, devolver el último error encontrado
+            res.end(error);
         }
-    )
+    } catch (error) {
+        console.log(error)
+        // Manejar cualquier error inesperado durante la consulta.
+        res.end(`0${split}Se ha presentado un error debido a ${error.message}. Por favor inténtelo más tarde`);
+    }
 })
